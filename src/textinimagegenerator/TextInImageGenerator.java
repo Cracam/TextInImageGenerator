2b2bc4b6ab95446154dc0e211daa6108de1c0674
camil <camil@PC_Camille>
2025-01-11 12:28:27 +0100

    Change the parameter of thext size from interface to layer 	(to allow multiple text size from only one layer)

--- src/textinimagegenerator/TextInImageGenerator.java
+++ src/textinimagegenerator/TextInImageGenerator.java
@@ -2,12 +2,13 @@ package textinimagegenerator;
 
 import Exeptions.ResourcesFileErrorException;
 import java.awt.Color;
+import java.awt.FontFormatException;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.beans.property.BooleanProperty;
@@ -18,9 +19,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -28,7 +27,7 @@ import javafx.stage.Stage;
  *
  * @author Camille LECOURT
  */
-public class TextInImageGenerator extends HBox {
+public class TextInImageGenerator extends VBox {
 
         @FXML
         private VBox root;
@@ -44,24 +43,29 @@ public class TextInImageGenerator extends HBox {
 
         @FXML
         private Slider textSizeSlideBar;
-        
+
         @FXML
         private Slider TextHeighSlideBar;
-        
+
         @FXML
-        private HBox FontChargerHBox;
+        private VBox FontChargerHBox;
+
+        @FXML
+        private VBox VboxTextSize;
+
+        @FXML
+        private VBox VboxTextHeight;
 
         private final BooleanProperty changed = new SimpleBooleanProperty(false);
 
-        private Font customFont =new Font("Arial", 14);
+        private java.awt.Font customFontAwt = new java.awt.Font("Arial", 0, 10);
+        private javafx.scene.text.Font customFontFX = new javafx.scene.text.Font("Arial", 10);
         private double oldFontSize = 14.0;
         private boolean fontChanged = false;
-        
-        private static final String PLACEHOLDER_TEXT="Veuillez Remplacer ce Texte";
-        
+
+        private static final String PLACEHOLDER_TEXT = "Veuillez Remplacer ce Texte";
+
         private File fontFile;
-        
-      
 
         public TextInImageGenerator() {
                 try {
@@ -87,27 +91,24 @@ public class TextInImageGenerator extends HBox {
                 textSizeSlideBar.valueProperty().addListener((observable, oldValue, newValue) -> {
                         refreshFont();
                 });
-                setTextSizeSlideBar(5,40,1);
-                textSizeSlideBar.setValue(12.0);
-                
-                 TextHeighSlideBar.valueProperty().addListener((observable, oldValue, newValue) -> {
+                setTextSizeSlideBar(0, 1, 0.05);
+                textSizeSlideBar.setValue(0.5);
+
+                TextHeighSlideBar.valueProperty().addListener((observable, oldValue, newValue) -> {
                 });
-                 setTextHeigthSlideBar(-1,1,  0.05);
+                setTextHeigthSlideBar(-1, 1, 0.05);
                 TextHeighSlideBar.setValue(0.0);
-                
 
                 fontSelectorButton.setOnAction(event -> {
                         // Handle button click event
                         loadPolicie();
                         refreshFont();
                 });
-                
-                
-                        
+
                 textField.focusedProperty().addListener((observable, oldValue, newValue) -> {
                         if (textField.getText().isEmpty()) {
                                 textField.setText(PLACEHOLDER_TEXT);
-                               refreshText();
+                                refreshText();
                         }
                 });
 
@@ -120,24 +121,25 @@ public class TextInImageGenerator extends HBox {
         private void refreshText() {
                 String text = textField.getText();
                 PolicePreview.setText(text);
+                setChanged(true);
         }
 
         /**
          * Refrash the font and the size of the text
          */
         private void refreshFont() {
-                if (customFont != null & (fontChanged == true | (oldFontSize != textSizeSlideBar.getValue()))) {
-                        if (oldFontSize != textSizeSlideBar.getValue()) {
-                                customFont = changeFontSize(customFont, textSizeSlideBar.getValue());
-                        }
-                        oldFontSize = textSizeSlideBar.getValue();
-                        PolicePreview.setFont(customFont);
+                if (customFontFX != null & fontChanged == true ) {
+                        PolicePreview.setFont(customFontFX);
                         fontChanged = false;
                 }
         }
 
         public void setChanged(boolean value) {
                 this.changed.set(value);
+        }
+
+        public BooleanProperty isChanged() {
+                return changed;
         }
 
         /**
@@ -158,42 +160,54 @@ public class TextInImageGenerator extends HBox {
          * Desactivate the text field
          */
         public void desactivateTextSizeSlideBar() {
-                textSizeSlideBar.setDisable(true);
+                VboxTextSize.setDisable(true);
+                VboxTextSize.setVisible(false);
+                VboxTextSize.setManaged(false);
         }
 
         /**
          * Activate the text field
          */
         public void activateTextSizeSlideBar() {
-                textSizeSlideBar.setDisable(false);
+                VboxTextSize.setDisable(false);
+                VboxTextSize.setVisible(true);
+                VboxTextSize.setManaged(true);
+
         }
 
-        /**
-         * Desactivate thePoliceSelector
-         */
-        public void desactivatePoliceSelector() {
-                fontSelectorButton.setDisable(true);
-        }
-
-        /**
-         * Activate the PoliceSelector
-         */
-        public void activatePoliceSelector() {
-                fontSelectorButton.setDisable(false);
-        }
-        
         /**
          * This function hide the Font charger HBox
          */
-        public void hideFontCharger(){
+        public void desactivateFontCharger() {
                 FontChargerHBox.setVisible(false);
+                FontChargerHBox.setDisable(true);
+                                FontChargerHBox.setManaged(false);
+
+
         }
-        
+
         /**
          * This function show the Font charger HBox
          */
-        public void showFontCharger(){
+        public void activateFontCharger() {
                 FontChargerHBox.setVisible(true);
+                FontChargerHBox.setDisable(false);
+                FontChargerHBox.setManaged(true);
+
+        }
+
+        public void desactivateTextHeighSlideBar() {
+                VboxTextHeight.setVisible(false);
+                VboxTextHeight.setDisable(true);
+                                VboxTextHeight.setManaged(false);
+
+        }
+
+        public void activateTextHeighSlideBar() {
+                VboxTextHeight.setVisible(true);
+                VboxTextHeight.setDisable(false);
+                VboxTextHeight.setManaged(true);
+
         }
 
         /**
@@ -203,155 +217,180 @@ public class TextInImageGenerator extends HBox {
          * @param max
          * @param step
          */
-        private static void setSlideBar(Slider slideBar ,int min, int max, double step) {
+        private static void setSlideBar(Slider slideBar, int min, int max, double step) {
                 slideBar.setMin(min);
                 slideBar.setMax(max);
                 slideBar.setMajorTickUnit(step);
         }
-        
-        /**
-         * set Text Size Slide bar parameter 
-         * @param min
-         * @param max
-         * @param step 
-         */
-        public void setTextSizeSlideBar(int min, int max, double step){
-                setSlideBar(this.textSizeSlideBar,min,max,step);
-        }
-        
-            /**
-         * set Text Size Slide bar parameter 
-         * @param min
-         * @param max
-         * @param step 
-         */
-        public void setTextHeigthSlideBar(int min, int max, double step){
-                setSlideBar(this.TextHeighSlideBar,min,max,step);
-        }
-        
 
         /**
-         * Load a text policie file (.ttf add other if you want)
+         * set Text Size Slide bar parameter
+         *
+         * @param min
+         * @param max
+         * @param step
          */
-        public void loadPolicie() {
-                FileChooser fileChooser = new FileChooser();
-                fileChooser.setTitle("Select Font File");
-                fileChooser.getExtensionFilters().addAll(
-                        new FileChooser.ExtensionFilter("Font Files", "*.ttf", "*.otf")
-                );
-
-                File selectedFile = fileChooser.showOpenDialog(new Stage());
-                if (selectedFile != null) {
-                        try {
-                                fontFile=selectedFile;
-                                //    System.out.println(selectedFile.toURL().toString());
-                                customFont = Font.loadFont(selectedFile.toURL().toString(), textSizeSlideBar.getValue());
-                                if (customFont != null) {
-                                        PolicePreview.setFont(customFont);
-                                        fontChanged = true;
-                                        refreshFont();
-                                        refreshText();
-                                }
-                        } catch (MalformedURLException e) {
-                                Logger.getLogger(TextInImageGenerator.class.getName()).log(Level.SEVERE, "Exception while loading font: " + e.getMessage());
-                        }
-                }
+        public void setTextSizeSlideBar(int min, int max, double step) {
+                setSlideBar(this.textSizeSlideBar, min, max, step);
         }
 
-        public static Font changeFontSize(Font customFont, double newSize) {
+        /**
+         * set Text Size Slide bar parameter
+         *
+         * @param min
+         * @param max
+         * @param step
+         */
+        public void setTextHeigthSlideBar(int min, int max, double step) {
+                setSlideBar(this.TextHeighSlideBar, min, max, step);
+        }
+
+        /**
+ * Load a text policie file (.ttf add other if you want)
+ */
+public void loadPolicie() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Select Font File");
+    fileChooser.getExtensionFilters().addAll(
+            new FileChooser.ExtensionFilter("Font Files", "*.ttf", "*.otf")
+    );
+
+    File selectedFile = fileChooser.showOpenDialog(new Stage());
+    if (selectedFile != null) {
+       loadNewFont(selectedFile);
+    }
+}
+
+public void loadNewFont(File selectedFile){
+         try {
+            fontFile = selectedFile;
+            // Load the font using AWT
+            java.awt.Font awtFont = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, selectedFile);
+            awtFont = awtFont.deriveFont((float) 10.0);
+
+            // Convert AWT font to JavaFX font
+            customFontAwt = awtFont;
+            customFontFX = javafx.scene.text.Font.loadFont(selectedFile.toURI().toString(), 10);
+
+            if (customFontFX != null) {
+                PolicePreview.setFont(customFontFX);
+                fontChanged = true;
+              
+            }
+            refreshFont();
+                refreshText();
+        } catch (FontFormatException | IOException e) {
+            Logger.getLogger(TextInImageGenerator.class.getName()).log(Level.SEVERE, "Exception while loading font: " + e.getMessage());
+        }
+}
+
+
+        public static java.awt.Font changeFontSize(java.awt.Font customFont, double newSize) {
                 // Create a new font with the same family but new size
-                return new Font(customFont.getFamily(), newSize);
+            ///    return new java.awt.Font(customFont.getFamily(),PLAIN , (int) newSize);
+                return  customFont.deriveFont((float) newSize);
+
         }
-        
-        
-        public File getFontFile(){
+
+
+        public File getFontFile() {
                 return this.fontFile;
         }
-       
+
         /**
          * return the text write by the user
-         * @return 
-         */
-        public String getTextValue(){
-                return this.textField.getText();
-        }
-        
-        /**
-         * return the size of the text selected by the user
-         * @return 
-         */
-        public double getTextSizeValue(){
-                return this.textSizeSlideBar.getValue();
-        }
-        
-        /**
-         * return the heigth modifier selected by the user
-         * @return 
-         */
-        public double getTextHeigthValue(){
-                return this.TextHeighSlideBar.getValue();
-        }
-        
-        
-         /**
-     * Generate a BufferedImage with the specified dimensions and an opacity of 0 everywhere except for the text.
-     *
-     * @param dim_x The width of the image.
-     * @param dim_y The height of the image.
-     * @return A BufferedImage with the text drawn on it.
-     */
-    public BufferedImage getImageOut(int dim_x, int dim_y) {
-        BufferedImage image = new BufferedImage(dim_x, dim_y, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = image.createGraphics();
-
-        // Set rendering hints for better quality
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
-        // Clear the image with transparent background
-        g2d.setComposite(java.awt.AlphaComposite.Clear);
-        g2d.fillRect(0, 0, dim_x, dim_y);
-        g2d.setComposite(java.awt.AlphaComposite.SrcOver);
-
-        // Set the font and color
-        g2d.setFont(convertToAwtFont(customFont));
-        g2d.setColor(Color.BLACK);
-
-        // Draw the text
-        String text = textField.getText();
-        int textWidth = g2d.getFontMetrics().stringWidth(text);
-        int textHeight = g2d.getFontMetrics().getAscent();
-        int x = (dim_x - textWidth) / 2;
-        int y = (dim_y + textHeight) / 2;
-        g2d.drawString(text, x, y);
-
-        // Dispose the graphics context
-        g2d.dispose();
-
-        return image;
-    }
-    
-        /**
-         * Convert javaFX font to java awt font
          *
-         * @param javafxFont
          * @return
          */
-        public static java.awt.Font convertToAwtFont(javafx.scene.text.Font javafxFont) {
-                String family = javafxFont.getFamily();
-                float size = (float) javafxFont.getSize();
-
-                int style = java.awt.Font.PLAIN;
-
-                // Determine the style based on FontWeight and FontPosture
-                if (javafxFont.getStyle().equals("Bold")) {
-                        style = java.awt.Font.BOLD;
-                } else if (javafxFont.getStyle().equals("Italic")) {
-                        style = java.awt.Font.ITALIC;
-                } else if (javafxFont.getStyle().equals("Bold Italic")) {
-                        style = java.awt.Font.BOLD | java.awt.Font.ITALIC;
-                }
-
-                return new java.awt.Font(family, style, (int) size);
+        public String getTextValue() {
+                return this.textField.getText();
         }
+
+        /**
+         * return the size of the text selected by the user
+         *
+         * @return
+         */
+        public double getTextSizeValue() {
+                return this.textSizeSlideBar.getValue();
+        }
+
+        /**
+         * return the heigth modifier selected by the user
+         *
+         * @return
+         */
+        public double getTextHeigthValue() {
+                return this.TextHeighSlideBar.getValue();
+        }
+
+        public void loadValues(String text, double textSize, double textHeightModifier) {
+                this.textField.setText(text);
+                this.TextHeighSlideBar.setValue(textHeightModifier);
+                this.textSizeSlideBar.setValue(textSize);
+        }
+
+        /**
+         * Generate a BufferedImage with the specified dimensions and an opacity
+         * of 0 everywhere except for the text.
+         *
+         * @param size_factor The factor by which to scale the text size to IN
+         * pixel determine the image dimensions.
+         * @param textSizeMin
+         * @param textSizeMax
+         * @return A BufferedImage with the text drawn on it.
+         */
+        public BufferedImage getImageOut(float size_factor, float textSizeMin, float textSizeMax) {
+
+                // Create a temporary BufferedImage to measure the text size
+                BufferedImage tempImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+                Graphics2D tempG2d = tempImage.createGraphics();
+
+                // Set rendering hints for better quality
+                tempG2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                tempG2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+                // Set the font
+                System.out.println("size_factor :   " + size_factor);
+                double textSize= (textSizeMin+textSizeSlideBar.getValue()*(textSizeMax-textSizeMin)) * size_factor* 72 / 96;
+                tempG2d.setFont(changeFontSize(customFontAwt, textSize));
+
+                // Measure the text size
+                String text = textField.getText();
+                 FontMetrics fontMetrics = tempG2d.getFontMetrics();
+                int textWidth = fontMetrics.stringWidth(text);
+                if (textWidth == 0) {
+                        textWidth = 2;
+                }
+                int textHeight = fontMetrics.getHeight();
+                        
+                // Dispose the temporary graphics context
+                tempG2d.dispose();
+
+
+                // Create the final BufferedImage with the calculated dimensions
+                BufferedImage image = new BufferedImage((int) (textWidth), (int) (textHeight * 2), BufferedImage.TYPE_INT_ARGB);
+                Graphics2D g2d = image.createGraphics();
+
+                // Set rendering hints for better quality
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+                // Clear the image with transparent background
+                g2d.setComposite(java.awt.AlphaComposite.Clear);
+                g2d.fillRect(0, 0, textWidth, textHeight);
+                g2d.setComposite(java.awt.AlphaComposite.SrcOver);
+
+                // Set the font and color
+                g2d.setFont(changeFontSize(customFontAwt,textSize));
+                g2d.setColor(Color.BLACK);
+
+                // Draw the text centered in the image
+                g2d.drawString(text, 0, (int) (textHeight));
+
+                // Dispose the graphics context
+                g2d.dispose();
+
+                return image;
+                }
 }
